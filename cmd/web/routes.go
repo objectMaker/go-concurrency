52fda2aadc64387c90adfd913675c52175fe8cae
于邦飞 <528154320@qq.com>
2024-10-16 17:15:24 +0800

    add login and register page

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,5 +13,13 @@ func (app *Config) routes() http.Handler {
 	mux.Use(app.SessionLoad)
 
 	mux.Get("/", app.HomePage)
+
+	mux.Get("/login", app.LoginPage)
+	mux.Post("/login", app.PostLoginPage)
+
+	mux.Get("/logout", app.Logout)
+	mux.Get("/register", app.RegisterPage)
+	mux.Post("/register", app.PostRegisterPage)
+	mux.Get("/activate-account", app.ActivateAccount)
 	return mux
 }
