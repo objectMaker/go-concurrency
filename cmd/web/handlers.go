e230067f90a2cfde500ea76ba12aa650391e73b8
于邦飞 <528154320@qq.com>
2024-10-17 16:54:11 +0800

    add login logic

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -20,8 +20,28 @@ func (app *Config) PostLoginPage(w http.ResponseWriter, r *http.Request) {
 	email := r.Form.Get("email")
 	password := r.Form.Get("password")
 
-	user,err := app.Models.
+	user, err := app.Models.User.GetByEmail(email)
 
+	if err != nil {
+		app.Session.Put(r.Context(), "error", "Invalid credentials")
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+	validPassword, err := user.PasswordMatches(password)
+	if err != nil {
+		app.Session.Put(r.Context(), "error", "Invalid credentials")
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+	if !validPassword {
+		app.Session.Put(r.Context(), "error", "Invalid credentials")
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+	app.Session.Put(r.Context(), "userID", user.ID)
+	app.Session.Put(r.Context(), "user", user)
+	app.Session.Put(r.Context(), "flash", "success to login")
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func (app *Config) Logout(w http.ResponseWriter, r *http.Request) {
